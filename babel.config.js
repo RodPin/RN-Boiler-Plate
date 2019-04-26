37ff75a339dc6d9c28cf980733403a6a4c750577
Rodrigo Gomes <prodrigo@poli.ufrj.br>
2019-04-26 12:23:13 -0300

    navigation adjusts

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"]
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./src"],
+        alias: {
+          "@components": "RNboilerplate/src/components",
+          "@navigators": "RNboilerplate/src/navigators",
+          "@reducers": "RNboilerplate/src/reducers",
+          "@res": "RNboilerplate/src/res",
+          "@sagas": "RNboilerplate/src/sagas",
+          "@services": "RNboilerplate/src/services",
+          "@themes": "RNboilerplate/src/themes",
+          "@views": "RNboilerplate/src/views",
+          "@store": "RNboilerplate/src/store"
+        }
+      }
+    ]
+  ],
+  env: {
+    production: {
+      plugins: ["react-native-paper/babel"]
+    }
+  }
 };
---
+++ src/navigators/RootNavigator.js
@@ -0,0 +1,11 @@
+// import { createAppContainer, createSwitchNavigator } from "react-navigation";
+
+// import MainNavigator from "@navigators/MainNavigator";
+// import AuthNavigator from "@navigators/AuthNavigator";
+
+// export default createAppContainer(
+//   createSwitchNavigator({
+//     Main: MainNavigator,
+//     Auth: AuthNavigator
+//   })
+// );
